f403eca5fac542d32b3d6ceab1e17b5724d3183c
Timothy E Traylor <ttraylor83@gmail.com>
2022-10-24 14:14:26 -0700

    more base code stuff

---
+++ src/Components/header/header.test.jsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
+
+
+// import Header from './Header';
+
+// describe('The Header Component', () => {
+//   test('Renders h1 with props as expected', () => {
+//     render(<Header checkin="Test"/>);
+
+//     let h2 = screen.getByTestId('header-h2');
+//     expect(h2).toHaveTextContent('What is this?');
+
+//     let anotherEx = screen.getByText('TESTing?');
+//     expect(anotherEx).toBeTruthy();
+//     expect(anotherEx).toBeInTheDocument();
+
+//   });
+
+
+// });
