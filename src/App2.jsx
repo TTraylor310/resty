b1bc3df78cd93263dc6797c776fa579bcd5b6f95
Timothy E Traylor <ttraylor83@gmail.com>
2022-10-25 21:15:53 -0700

    passing state down and actions up

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-
+import React from 'react';
 import Header from './Components/header/header';
 import Footer from './Components/footer/footer';
 import Form from './Components/form/form';
@@ -8,38 +7,39 @@ import Results from './Components/results/results';
 import './App.scss';
 
 
-const App = () => {
-
-  const [dataState, setDataState] = useState(
-    {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       data: null,
       requestParams: {},
-    }
-  )
+    };
+  }
 
-  const callApi = (requestParams) => {
-    const newData = {
+  callApi = (requestParams) => {
+    const data = {
       count: 2,
       results: [
         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
       ],
     };
-    setDataState(newData, requestParams);
+    this.setState({data, requestParams})
   }
 
-  return (
+  render(){
+    return (
       <div className="App">
         <Header />
-        {/* <div>Request Method: {dataState.requestParams.method}</div> */}
-        {/* <div>URL: {dataState.requestParams.url}</div> */}
-        <Form fakeAPI={callApi} />
-        <Results fakeData={dataState.data}/>
+        <div>Request Method: {this.state.requestParams.method}</div>
+        <div>URL: {this.state.requestParams.url}</div>
+        <Form fakeAPI={this.callApi}/>
+        <Results fakeData={this.state.data}/>
         <Footer />
       </div>
-  )
+    )
 
+  }
 }
-
 
 export default App;
